c5d12b9034b4dc4ec40b67a4f770b5e3a022ff95
Josh Beard <josh@signalboxes.net>
2015-05-19 08:18:06 -0600

    Expand rspec tests

--- manifests/init.pp
+++ manifests/init.pp
@@ -19,17 +19,17 @@ class login_defs (
 ) inherits login_defs::params {
 
   validate_absolute_path($mail_dir)
-  validate_re($pass_max_days, '^\d+$')
-  validate_re($pass_min_days, '^\d+$')
-  validate_re($pass_min_len, '^\d+$')
-  validate_re($pass_warn_age, '^\d+$')
-  validate_re($uid_min, '^\d+$')
-  validate_re($uid_max, '^\d+$')
-  validate_re($gid_min, '^\d+$')
-  validate_re($gid_max, '^\d+$')
-  validate_re($create_home, '^(yes|no)$')
-  validate_re($umask, '^\d{3,4}$')
-  validate_re($usergroups_enab, '^(yes|no)$')
+  validate_re($pass_max_days, '^\d+$', 'pass_max_days must be a number')
+  validate_re($pass_min_days, '^\d+$', 'pass_min_days must be a number')
+  validate_re($pass_min_len, '^\d+$', 'pass_min_len must be a number')
+  validate_re($pass_warn_age, '^\d+$', 'pass_warn_age must be a number')
+  validate_re($uid_min, '^\d+$', 'uid_min must be a number')
+  validate_re($uid_max, '^\d+$', 'uid_max must be a number')
+  validate_re($gid_min, '^\d+$', 'gid_min must be a number')
+  validate_re($gid_max, '^\d+$', 'gid_max must be a number')
+  validate_re($create_home, '^(yes|no)$', 'create_home must be yes or no')
+  validate_re($umask, '^\d{3,4}$', 'umask must be a 3-4 digit number')
+  validate_re($usergroups_enab, '^(yes|no)$', 'usergroups_enab must be yes or no')
   validate_hash($options)
 
   file { '/etc/login.defs':
--- spec/classes/init_spec.rb
+++ spec/classes/init_spec.rb
@@ -20,12 +20,23 @@ describe 'login_defs' do
     end
   end
 
-  context 'when called with custom parameters' do
-    let(:facts) {
+  context 'when called with custom template' do
+    let(:params) {
       {
-        :osfamily => 'RedHat',
+        :template => 'myorg/login.defs.erb',
       }
     }
+
+    it { should contain_file('/etc/login.defs').with_content(
+      /^CUSTOM_TEMPLATE\s+yes$/
+    )}
+
+    it { should contain_file('/etc/login.defs').with_content(
+      /^PASS_MAX_DAYS\s+99999$/
+    )}
+  end
+
+  context 'when called with custom parameters' do
 
     let(:params) {
       {
@@ -54,67 +65,149 @@ describe 'login_defs' do
       should contain_file('/etc/login.defs').with_content(
         /^MAIL_DIR\s+\/tmp\/foo$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^PASS_MAX_DAYS\s+89$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^PASS_MIN_DAYS\s+79$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^PASS_MIN_LEN\s+16$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^PASS_WARN_AGE\s+14$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^UID_MIN\s+420$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^UID_MAX\s+50000$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^GID_MIN\s+419$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^GID_MAX\s+49999$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^USERDEL_CMD\s+\/usr\/bin\/foo$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^CREATE_HOME\s+no$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^UMASK\s+077$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^USERGROUPS_ENAB\s+no$/
       )
+    end
 
+    it do
       should contain_file('/etc/login.defs').with_content(
         /^ENCRYPT_METHOD\s+SHA256$/
       )
-
-      should contain_file('/etc/login.defs').with_content(
-        /^FAIL_DELAY\s+5$/
-      )
-
-      should contain_file('/etc/login.defs').with_content(
-        /^MD5_CRYPT_ENAB\s+no$/
-      )
     end
+
+  end
+
+  context 'when called with bad parameter values' do
+    context 'when called with invalid mail_dir' do
+      let(:params) { { :mail_dir => 'invalid' } }
+      it { should raise_error( Puppet::Error, /"invalid" is not an absolute path/ ) }
+    end
+
+    context 'when called with invalid pass_max_days' do
+      let(:params) { { :pass_max_days => 'foo' } }
+      it { should raise_error( Puppet::Error, /pass_max_days must be a number/ ) }
+    end
+
+    context 'when called with invalid pass_min_days' do
+      let(:params) { { :pass_min_days => 'foo' } }
+      it { should raise_error( Puppet::Error, /pass_min_days must be a number/ ) }
+    end
+
+    context 'when called with invalid pass_min_len' do
+      let(:params) { { :pass_min_len => 'foo' } }
+      it { should raise_error( Puppet::Error, /pass_min_len must be a number/ ) }
+    end
+
+    context 'when called with invalid pass_warn_age' do
+      let(:params) { { :pass_warn_age => 'foo' } }
+      it { should raise_error( Puppet::Error, /pass_warn_age must be a number/ ) }
+    end
+
+    context 'when called with invalid uid_min' do
+      let(:params) { { :uid_min => 'foo' } }
+      it { should raise_error( Puppet::Error, /uid_min must be a number/ ) }
+    end
+
+    context 'when called with invalid uid_max' do
+      let(:params) { { :uid_max => 'foo' } }
+      it { should raise_error( Puppet::Error, /uid_max must be a number/ ) }
+    end
+
+    context 'when called with invalid gid_min' do
+      let(:params) { { :gid_min => 'foo' } }
+      it { should raise_error( Puppet::Error, /gid_min must be a number/ ) }
+    end
+
+    context 'when called with invalid gid_max' do
+      let(:params) { { :gid_max => 'foo' } }
+      it { should raise_error( Puppet::Error, /gid_max must be a number/ ) }
+    end
+
+    context 'when called with invalid create_home' do
+      let(:params) { { :create_home => 'definitely' } }
+      it { should raise_error( Puppet::Error, /create_home must be yes or no/ ) }
+    end
+
+    context 'when called with invalid umask' do
+      let(:params) { { :umask => '07700' } }
+      it { should raise_error( Puppet::Error, /umask must be a 3-4 digit number/ ) }
+    end
+
+    context 'when called with invalid usergroups_enab' do
+      let(:params) { { :usergroups_enab => 'absolutely' } }
+      it { should raise_error( Puppet::Error, /usergroups_enab must be yes or no/ ) }
+    end
+
   end
 
 end

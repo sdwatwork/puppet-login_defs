721735ea5568ea593c07f94601ff8ca13898bd77
RJ Lalumiere <rj.lalumiere@dealer.com>
2016-04-06 11:02:44 -0400

    Update to account for changes in Red Hat 7

--- Readme.md
+++ Readme.md
@@ -73,7 +73,7 @@ class { '::login_defs':
 
 __Default Options:__
 
-__RedHat:__
+__RedHat 5/6:__
 
 ```ruby
 {
@@ -88,6 +88,30 @@ __RedHat:__
 'UID_MAX'         => '60000',
 'UID_MIN'         => '500',
 'UMASK'           => '022',
+'USERGROUPS_ENAB' => 'yes',
+}
+```
+
+__RedHat 7:__
+
+```ruby
+{
+'CREATE_HOME'     => 'yes',
+'ENCRYPT_METHOD'  => 'SHA512',
+'GID_MAX'         => '60000',
+'GID_MIN'         => '1000',
+'MAIL_DIR'        => '/var/spool/mail',
+'PASS_MAX_DAYS'   => '99999',
+'PASS_MIN_DAYS'   => '0',
+'PASS_MIN_LEN'    => '5',
+'PASS_WARN_AGE'   => '7',
+'SYS_GID_MAX'     => '999',
+'SYS_GID_MIN'     => '201',
+'SYS_UID_MAX'     => '999',
+'SYS_UID_MIN'     => '201',
+'UID_MAX'         => '60000',
+'UID_MIN'         => '1000',
+'UMASK'           => '077',
 'USERGROUPS_ENAB' => 'yes',
 }
 ```
--- manifests/params.pp
+++ manifests/params.pp
@@ -1,21 +1,48 @@
+# Default values for /etc/login.defs .
+# These can be overridden or added to by passing a hash to the $options parameter in the main class.
 #
 class login_defs::params {
 
   case $::osfamily {
     'RedHat': {
-      $default_options = {
-        'CREATE_HOME'     => 'yes',
-        'ENCRYPT_METHOD'  => 'SHA512',
-        'GID_MAX'         => '60000',
-        'GID_MIN'         => '500',
-        'MAIL_DIR'        => '/var/spool/mail',
-        'PASS_MAX_DAYS'   => '99999',
-        'PASS_MIN_LEN'    => '5',
-        'PASS_WARN_AGE'   => '7',
-        'UID_MAX'         => '60000',
-        'UID_MIN'         => '500',
-        'UMASK'           => '022',
-        'USERGROUPS_ENAB' => 'yes',
+      case $::operatingsystemmajrelease {
+        '7': {
+          $default_options = {
+            'CREATE_HOME'     => 'yes',
+            'ENCRYPT_METHOD'  => 'SHA512',
+            'GID_MAX'         => '60000',
+            'GID_MIN'         => '1000',
+            'MAIL_DIR'        => '/var/spool/mail',
+            'PASS_MAX_DAYS'   => '99999',
+            'PASS_MIN_DAYS'   => '0',
+            'PASS_MIN_LEN'    => '5',
+            'PASS_WARN_AGE'   => '7',
+            'SYS_GID_MAX'     => '999',
+            'SYS_GID_MIN'     => '201',
+            'SYS_UID_MAX'     => '999',
+            'SYS_UID_MIN'     => '201',
+            'UID_MAX'         => '60000',
+            'UID_MIN'         => '1000',
+            'UMASK'           => '077',
+            'USERGROUPS_ENAB' => 'yes',
+          }
+        }
+        default: {
+          $default_options = {
+            'CREATE_HOME'     => 'yes',
+            'ENCRYPT_METHOD'  => 'SHA512',
+            'GID_MAX'         => '60000',
+            'GID_MIN'         => '500',
+            'MAIL_DIR'        => '/var/spool/mail',
+            'PASS_MAX_DAYS'   => '99999',
+            'PASS_MIN_LEN'    => '5',
+            'PASS_WARN_AGE'   => '7',
+            'UID_MAX'         => '60000',
+            'UID_MIN'         => '500',
+            'UMASK'           => '022',
+            'USERGROUPS_ENAB' => 'yes',
+          }
+        }
       }
     }
     'Debian': {
